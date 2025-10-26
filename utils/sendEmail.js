72a03a59dce79d1ab876c56f070aa888e46ca6a4
piyush rawat <rawatpiyush006@gmail.com>
2025-10-26 22:35:02 +0530

    DSF

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,18 +1,35 @@
+// import nodemailer from "nodemailer";
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS, // Gmail App Password
+//   },
+// });
+
+// export const sendOtpEmail = async (to, otp) => {
+//   await transporter.sendMail({
+//     from: `"TaskFlow" <${process.env.EMAIL_USER}>`,
+//     to,
+//     subject: "Your TaskFlow OTP Code",
+//     text: `Your OTP is ${otp}. It expires in 5 minutes.`,
+//   });
+// };
+
+
+
 import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for 465, false for 587
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // Gmail App Password
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
-
-export const sendOtpEmail = async (to, otp) => {
-  await transporter.sendMail({
-    from: `"TaskFlow" <${process.env.EMAIL_USER}>`,
-    to,
-    subject: "Your TaskFlow OTP Code",
-    text: `Your OTP is ${otp}. It expires in 5 minutes.`,
-  });
-};
